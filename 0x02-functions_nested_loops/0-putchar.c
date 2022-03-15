498c11ba8683db50dd44da3a68754ba8bf8986eb
bradleymatjie <bradleysaint45@gmail.com>
2022-03-15 10:33:41 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,11 +12,10 @@ int main(void)
 	int c;
 	char wrd[9] = "_putchar";
 
-	for (a = 0; i < 8; i++)
+	for (c = 0; c < 8; c++)
 	{
-		_putchar(wrd[i]);
+		_putchar(wrd[c]);
 	}
 	_putchar('\n');
-
 	return (0);
 }
