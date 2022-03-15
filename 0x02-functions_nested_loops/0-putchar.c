3e3e493b3ad816d12b1d34c3136bebaa69a6f401
bradleymatjie <bradleysaint45@gmail.com>
2022-03-15 10:15:15 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main- main block
+ * Description: this program prints out
+ * the string "_putchar" on the screen
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+	char wrd[9] = "_putchar";
+
+	for (a = 0; i < 8; i++)
+	{
+		_putchar(wrd[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
