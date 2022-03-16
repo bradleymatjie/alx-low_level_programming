4680706a79b1b1f0b11b6919936ba746846203db
bradleymatjie <bradleysaint45@gmail.com>
2022-03-16 09:39:46 -0700

    advanced tasks

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define LARGEST 10000000000
+/**
+ * main - main block
+ * Description: prints the first 98 fibonacci numbers,
+ * starting with 1 and 2, followered by a ne line.
+ * Return: (0)
+ */
+int main(void)
+{
+	unsigned long int fr1 = 0, bk1 = 1, m fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
+
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
+	{
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			print(", ");
+	}
+	printf("\n");
+	return (0);
+}
