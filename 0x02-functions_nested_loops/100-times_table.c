f457bd8c17e64d576e8abecfff8996099047562b
bradleymatjie <bradleysaint45@gmail.com>
2022-03-16 09:44:06 -0700

    advanced tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ void print_times_table(int n)
 	{
 		for (row = 0; row <= n; row++)
 		{
-			for  (column = 0; columns <= n; columns++)
+			for  (column = 0; column <= n; column++)
 			{
 				product = row * columns;
 
