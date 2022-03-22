67eff137679e9051d6b6fd006b2d01945a789e7a
bradleymatjie <bradleysaint45@gmail.com>
2022-03-22 10:06:39 -0700

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,13 +13,13 @@ void rev_string(char *s)
 	for (len = 0; s[len] != '\0'; len++)
 		;
 
-	i =0;
+	i = 0;
 	half = len / 2;
 
 	while (half--)
 	{
 		temp = s[len - i - 1];
-		s[len - i -1] = s[i];
+		s[len - i - 1] = s[i];
 		s[i] = temp;
 		i++;
 	}
