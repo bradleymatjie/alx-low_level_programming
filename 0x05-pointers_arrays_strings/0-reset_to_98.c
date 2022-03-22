bd44581e3c737c01f85a7783cec4fdc2a6444bd8
bradleymatjie <bradleysaint45@gmail.com>
2022-03-22 07:39:27 -0700

    task 0, 1 and 2

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes an int as parameter and
+ * updates the value to 98
+ * @n: the parameter to be changed
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
