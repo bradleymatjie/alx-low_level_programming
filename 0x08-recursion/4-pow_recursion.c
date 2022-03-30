79c255ed0c46ca4e20b271bd6eaa53e9651503e6
bradleymatjie <bradleysaint45@gmail.com>
2022-03-30 09:20:52 -0700

    5-sqrt

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,8 +9,12 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-		return (-1);
+	if (y >= 0)
+	{
+		if (y == 0)
+			return (1);
+		return (x * _pow_recursion(x, y - 1));
+	}
 	else
-		return (pow(x, y));
+		return (-1);
 }
