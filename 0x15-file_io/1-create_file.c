0fd0c24b7f20d7ff08dff8723cbc6bbe8a48f843
bradleymatjie <bradleysaint45@gmail.com>
2022-05-07 01:53:22 -0700

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		if (res_write == -1)
 		{
 			write(1, "fails", 6);
-			return (0);
+			return (-1);
 		}
 	}
 	close(fd);
